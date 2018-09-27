9bcc786fca3142ccfda7e1559c0ed2cb599c56cd
mrzcs <mrzcs@163.com>
2018-09-27 23:53:21 +0800

    update

--- p4.py
+++ p4.py
@@ -2,10 +2,23 @@
 import pandas as pd
 import quandl
 
-""" api_key = open('../quandlapikey.txt', 'r').read()
+api_key = open('../quandlapikey.txt', 'r').read()
+""" print(api_key)
 df = quandl.get("FMAC/HPI_TX", authtoken=api_key)
 
 print(df.head()) """
 
 fiddyStates = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
-print(fiddyStates)
+
+#this is a list
+#print(fiddyStates)
+# this is the first dataframe
+#print(fiddyStates[0])
+#this is the second column
+#print(fiddyStates[0][1])
+#this is the second line
+print(fiddyStates[0][1][1])
+
+for abbr in fiddyStates[0][1][1:]:
+    #print(abbr)
+    print("FMAC/HPI_"+str(abbr))
---
+++ p8.py
@@ -0,0 +1,41 @@
+# Pickling
+import pandas as pd
+import quandl
+import pickle
+
+api_key = open('../quandlapikey.txt', 'r').read()
+
+def stateList():
+    fiddyStates = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
+    return fiddyStates[0][1][1:]
+
+def grabInitialStateData():
+    main_df =pd.DataFrame()
+    states = stateList()
+
+    for abbr in states:
+        #print(abbr)
+        query = 'FMAC/HPI_' + str(abbr)
+
+        df = quandl.get(query,authtoken=api_key)
+        print(query)
+        df.columns = [str(abbr)]
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df)
+
+    pickle_out = open('fiddy_states.pickle', 'wb')
+    pickle.dump(main_df, pickle_out)
+    pickle_out.close()
+
+def readStateData():
+    pickle_in = open('fiddy_states.pickle','rb')
+    HPI_data = pickle.load(pickle_in)
+    print(HPI_data)
+
+#grabInitialStateData()
+#readStateData()
+HPI_data = pd.read_pickle('fiddy_states.pickle')
+print(HPI_data.head())
+
